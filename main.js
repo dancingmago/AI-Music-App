131f864f2364bd3c1c6207105a477f8d1d9f007a
dancingmago <73450506+dancingmago@users.noreply.github.com>
2021-08-30 18:49:44 -0700

    Ai Music App Part 2

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+var song1 = "";
+var song2 = "";
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
+}
+
 function setup(){
     canvas = createCanvas(750, 750);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 750, 750);
+
+}
+
+function play(){
+    song.play();
 }
